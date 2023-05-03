4ec953ff60d03386c7add1006586070aba2e1c17
doowee0509 <ngocduy000937@gmail.com>
2023-05-03 01:28:24 -0400

    file to handle scores and writing texts on screen

---
+++ UI.js
@@ -0,0 +1,48 @@
+export class UI {
+    constructor(game) {
+        this.game = game;
+        this.fontSize = 30;
+        this.fontFamily = 'Creepster';
+        this.livesImage = document.getElementById('lives');
+    }
+    draw(context) {
+        context.save();
+        context.shadowOffsetX = 2;
+        context.shadowOffsetY = 2;
+        context.shadowColor = 'white';
+        context.shadowBlur = 0;
+        context.font = this.fontSize + 'px ' + this.fontFamily;
+        context.textAlign = 'left';
+        context.fillStyle = this.game.fontColor;
+        // score 
+        context.fillText('Score: ' + this.game.score, 20, 50);
+        // draw lives
+        for (let i = 0; i < this.game.lives; i++) {
+            context.drawImage(this.livesImage, 25 * i + 20, 60, 25 ,25);
+        }
+        // game over messages
+        if (this.game.gameOver) {
+            context.textAlign = 'center';
+            context.font = this.fontSize * 2 + 'px ' + this.fontFamily;
+            if (this.game.score > 5) {
+                context.fillText('GAME OVER!', this.game.width * 0.5, this.game.height * 0.5 - 20);
+                context.font = this.fontSize * 0.7 + 'px ' + this.fontFamily;
+                context.fillText('What are creatures of the night afraid oof? YOU!!!', this.game.width * 0.5, this.game.height * 0.5 + 20);
+            } else {
+                context.fillText('Love at first bite?', this.game.width * 0.5, this.game.height * 0.5 - 20);
+                context.font = this.fontSize + 'px ' + this.fontFamily;
+                context.fillText('Nope. Better luck next time!', this.game.width * 0.5, this.game.height * 0.5 + 20);
+            }
+            var btn = document.createElement('div');
+            btn.id = "play-btn-3";
+            btn.className = "btn";
+            btn.innerHTML = "Play Again"
+            document.getElementsByTagName('body')[0].prepend(btn);
+            document.getElementById('play-btn-3').style.zIndex = 10;
+            document.getElementById('play-btn-3').addEventListener('click', () => {
+                window.location.reload();
+            })
+        }
+        context.restore();
+    }
+}
